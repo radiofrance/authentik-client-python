6bdbed17cfa0100a238e34788e68a940b7badc0a
Alexandre NICOLAIE <alexandre.nicolaie@gmail.com>
2023-08-28 14:37:04 +0200

    Update API client (2023.6.1)

---
+++ authentik/api/__init__.py
@@ -0,0 +1,21 @@
+# flake8: noqa
+
+# import apis into api package
+from authentik.api.admin_api import AdminApi
+from authentik.api.authenticators_api import AuthenticatorsApi
+from authentik.api.core_api import CoreApi
+from authentik.api.crypto_api import CryptoApi
+from authentik.api.enterprise_api import EnterpriseApi
+from authentik.api.events_api import EventsApi
+from authentik.api.flows_api import FlowsApi
+from authentik.api.managed_api import ManagedApi
+from authentik.api.oauth2_api import Oauth2Api
+from authentik.api.outposts_api import OutpostsApi
+from authentik.api.policies_api import PoliciesApi
+from authentik.api.propertymappings_api import PropertymappingsApi
+from authentik.api.providers_api import ProvidersApi
+from authentik.api.root_api import RootApi
+from authentik.api.schema_api import SchemaApi
+from authentik.api.sources_api import SourcesApi
+from authentik.api.stages_api import StagesApi
+
---
+++ authentik/docs/UserTypeEnum.md
@@ -0,0 +1,11 @@
+# UserTypeEnum
+
+* `internal` - Internal * `external` - External * `service_account` - Service Account * `internal_service_account` - Internal Service Account
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
